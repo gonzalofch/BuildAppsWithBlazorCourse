7230b6f8446289b452776d1d47bd207b087aa620
Gonzalo Flores Chuchon <gonzalo.flores@ext.semicrol.com>
2024-05-17 13:50:43 +0200

    Blazor Course - Bind controls to data in Blazor applications -Bind elements to specific events

---
+++ DataAccessBlazor/DataAccessBlazor/Pages/EmailBindExample.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataAccessBlazor.Pages
+{
+    public partial class EmailBindExample : ComponentBase
+    {
+
+    }
+}

0afc54efeabdc8916fca1f0bff91700973f4fc25
Gonzalo Flores Chuchon <gonzalo.flores@ext.semicrol.com>
2024-05-23 16:39:07 +0200

    Blazor Course - Attach C# code to DOM events with Blazor event handlers - Use an EventCallback to handle events across components

---
+++ DataAccessBlazor/DataAccessBlazor/Pages/Data/KeyTransformation.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAccessBlazor.Pages.Data
+{
+    public class KeyTransformation
+    {
+        public string Key { get; set; }
+        public string TransformedKey { get; set; }
+    }
+}

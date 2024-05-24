a14cc24244de2b32786c1fe41eeab4d52a9cca9a
Gonzalo Flores Chuchon <gonzalo.flores@ext.semicrol.com>
2024-05-24 10:22:08 +0200

    Blazor Course - Exercise - Validate user input implicitly without writing validation code - Validate user input in Blazor forms & Prepare models for validation (dataannotationvalidation)

---
+++ DataAccessBlazor/DataAccessBlazor/Pages/FormsValidation/Pizza.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessBlazor.Pages.FormsValidation
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [EmailAddress]
+        public string ChefEmail { get; set; }
+
+        [Required]
+        [Range(10.00, 25.00)]
+        public decimal Price { get; set; }
+    }
+}

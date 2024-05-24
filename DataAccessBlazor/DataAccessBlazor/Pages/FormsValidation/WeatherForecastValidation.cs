cde0bab53e676b1dd249c99596349c5fa04ea63e
Gonzalo Flores Chuchon <gonzalo.flores@ext.semicrol.com>
2024-05-24 09:53:33 +0200

    Blazor Course - Take advantage of the power of Blazor forms - Handle form submission

---
+++ DataAccessBlazor/DataAccessBlazor/Pages/FormsValidation/WeatherForecastValidation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessBlazor.Pages.FormsValidation
+{
+    public class WeatherForecastValidation
+    {
+        [Required(ErrorMessage = "La fecha es obligatoria.")]
+        public DateTime Date { get; set; }
+
+        [Range(-100, 100, ErrorMessage = "La temperatura debe estar entre -100 y 100 grados Celsius.")]
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        [Required(ErrorMessage = "El resumen es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El resumen no debe exceder los 100 caracteres.")]
+        public string Summary { get; set; }
+    }
+}
